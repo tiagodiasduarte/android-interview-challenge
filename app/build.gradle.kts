c2237140c40dcb013f09be655facc513939507b3
Tiago Duarte <tiago.dias.duarte@gmail.com>
2026-09-24 16:31:01 +0100

    chore(build): remove unused instrumented test dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -109,10 +109,6 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
 
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(libs.androidx.junit)
-
     testImplementation(libs.androidx.compose.ui.test.junit4)
     testImplementation(libs.androidx.espresso.core)
     testImplementation(libs.androidx.junit)
